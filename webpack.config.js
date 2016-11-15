ec3451b3364a45553cf8d254b1d586d3adac5c4f
JCGrant <jamescolin.grant@gmail.com>
2016-11-15 03:03:10 +0000

    Create a basic Flask + React app skeleton

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var PATHS = {
+  src:  path.resolve(__dirname, 'client'),
+  dist:  path.resolve(__dirname, 'server/static/build'),
+};
+
+var config = {
+  entry: path.resolve(PATHS.src, 'main.js'),
+  output: {
+    path: PATHS.dist,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        include: PATHS.src,
+        loaders: [
+          'babel?cacheDirectory',
+        ],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+    ],
+  },
+};
+
+module.exports = config;
